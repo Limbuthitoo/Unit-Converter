bbdf1d9baf2e9115f8368d9104f555ca291cf339
Bijay Subba Limbu <bijaysubbalimbu@gmail.com>
2022-04-11 19:25:32 +0545

    change in input system

--- unit.js
+++ unit.js
@@ -1,23 +1,35 @@
-var change = 150.23956 ;
-document.getElementById("metric-el").textContent = change.toFixed(3);
+const filterInput = document.querySelector(".main #metric-el") ;
+// document.getElementById("metric-el").textContent = filterInput.toFixed(3);
 var mtofeet = 3.28084;
 var feettom = 0.3048;
 var ltogallon = 0.264172;
 var gallontol = 3.78541;
 var ktopound = 2.20462;
 var poundtok = 0.453592;
+let change;
+console.log(change);
 
-var meter2Feet = change * mtofeet;
-var feet2Meter = change * feettom;
-var liter2Gallon = change * ltogallon;
-var gallon2liter = change * gallontol;
-var kilo2Pound = change * ktopound;
-var pound2Kilo = change * poundtok;
+
 
 var lengthEl = document.getElementById("length-el");
 var volumeEl = document.getElementById("volume-el");
 var massEl = document.getElementById("mass-el");
+const checkBtn = document.querySelector(".btn button");
+filterInput.addEventListener("keyup",()=> {
+    change = filterInput.value.replace(/[^0-9]/gi,"");
+});
+checkBtn.addEventListener("click", () => {
+    var meter2Feet = (change * mtofeet).toFixed(3);
+    var feet2Meter = (change * feettom).toFixed(3);
+    var liter2Gallon = (change * ltogallon).toFixed(3);
+    var gallon2liter = (change * gallontol).toFixed(3);
+    var kilo2Pound = (change * ktopound).toFixed(3);
+    var pound2Kilo = (change * poundtok).toFixed(3);
+    lengthEl.textContent = change +" meters "+ " = " + meter2Feet + " feets " + " | " + change +" feets " + " = " + feet2Meter + " meters "
+    volumeEl.textContent = change + " liters " + " = " + liter2Gallon + " gallons " + " | " + change + " gallons " +" = " + gallon2liter + " liters "
+    massEl.textContent = change + " kilos " + " = " + kilo2Pound + " pounds " + " | " + change + " pounds " +" = " + pound2Kilo + " kilos "
+});
 
-lengthEl.textContent = change.toFixed(3) +" meters "+ " = " + meter2Feet.toFixed(3) + " feets " + " | " + change.toFixed(3) +" feets " + " = " + feet2Meter.toFixed(3) + " meters "
-volumeEl.textContent = change.toFixed(3) + " liters " + " = " + liter2Gallon.toFixed(3) + " gallons " + " | " + change.toFixed(3) + " gallons " + gallon2liter.toFixed(3) + " liters "
-massEl.textContent = change.toFixed(3) + " kilos " + " = " + kilo2Pound.toFixed(3) + " pounds " + " | " + change.toFixed(3) + " pounds " + pound2Kilo.toFixed(3) + " kilos "
+// lengthEl.textContent = filterInput.toFixed(3) +" meters "+ " = " + meter2Feet.toFixed(3) + " feets " + " | " + filterInput.toFixed(3) +" feets " + " = " + feet2Meter.toFixed(3) + " meters "
+// volumeEl.textContent = filterInput.toFixed(3) + " liters " + " = " + liter2Gallon.toFixed(3) + " gallons " + " | " + filterInput.toFixed(3) + " gallons " + gallon2liter.toFixed(3) + " liters "
+// massEl.textContent = filterInput.toFixed(3) + " kilos " + " = " + kilo2Pound.toFixed(3) + " pounds " + " | " + filterInput.toFixed(3) + " pounds " + pound2Kilo.toFixed(3) + " kilos "
